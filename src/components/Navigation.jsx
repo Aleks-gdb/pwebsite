f287ea1c360a2728a52dea46bcd47c279496c2bd
Aleks <jokerka17@gmail.com>
2019-06-20 17:28:51 -0700

    Details

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import {Navbar, Nav, NavItem} from "react-bootstrap";
 import AnchorLink from "react-anchor-link-smooth-scroll";
-/*The navigation component used for when there is no user logged in to the system*/
+
 export default class Navigation extends Component {
   render() {
     return (
